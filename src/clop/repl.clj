2194571dafbf3f8a302da1ee7d0e260b561b5a50
snozaki <snozaki@netprotections.co.jp>
2023-05-15 23:11:40 +0900

    fix REPLで例外をハンドリングする

--- src/clop/repl.clj
+++ src/clop/repl.clj
@@ -5,18 +5,26 @@
 
 ;; REPL。sigkillを受信するまで対話的環境を提供する
 
+(defn evaluate-script
+  []
+  (print "-> ")
+  (flush)
+  (let [newline (read-line)]
+    (if (= newline (or "exit" "quit"))
+      (println "Bye now!"))
+    (println (evaluate newline))))
+
 (defn start-repl
   []
   (println "Clop 0.1.0")
-  (while true
+  (loop []
     (do
-      (print "-> ")
-      (flush)
-      (let [newline (read-line)]
-        (if (= newline (or "exit" "quit"))
-          (println "Bye now!"))
-        (println (evaluate newline))
-        ))))
+      (try
+        (evaluate-script)
+        (catch Exception e
+          (do
+            (.printStackTrace e)
+            (evaluate-script)))))))
 
 (defn -main
   []
